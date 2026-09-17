f54b061ce081f8ac4d2beda7099a64de722d6a33
Michael Gerges <michael.george.mik@gmail.com>
2026-09-17 15:27:25 -0400

    made the main menu

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿bool exit = false;
+
+while (!exit)
+{
+    System.Console.WriteLine("\n=======================");
+    System.Console.WriteLine("| Welcome to the bank |");
+    System.Console.WriteLine("=======================");
+    System.Console.WriteLine("1. Customer");
+    System.Console.WriteLine("2. Admin");
+    System.Console.WriteLine("3. Exit");
+    System.Console.Write("Write a number to choose one of the options: ");
+    int userSelection = int.Parse(Console.ReadLine());
+    switch (userSelection)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            exit = true;
+            break;
+    }
+}
